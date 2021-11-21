5276ce67e56ec99169d510558c930268a6499b2f
israel.eguino204244@potros.itson.edu.mx <israel.eguino204244@potros.itson.edu.mx>
2021-11-21 01:05:14 -0700

    Backend apartar boleto

--- public/app.js
+++ public/app.js
@@ -41,3 +41,40 @@ const getBoletos = () => {
         return data;
     }); 
 }
+
+
+
+const apartarBoleto = (idBoleto, persona) => {
+    let boleto= getBoleto(idBoleto);
+    boleto.estadoBoleto= "APARTADO";
+    boleto.persona={
+        nombre: persona.nombre,
+        correo: persona.correo,
+        direccion: persona.direccion,
+        numTelefono: persona.numTelefono,
+        ciudad: persona.ciudad,
+        estado: persona.estado
+    }
+    fetch('/api/boleto/' + idBoleto, {
+        method: 'POST',
+        body: boleto
+    })
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        return data;
+    });
+}
+
+const getBoleto = (idBoleto) => {
+    fetch('/api/boleto'+ idBoleto, {
+        method: 'GET'
+    })
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        return data;
+    }); 
+}
